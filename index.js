e232601f3cd2ba8210352bda5932fceba648e0a2
Kevin Mullin <kevinmullinf@gmail.com>
2021-05-18 12:49:06 -0300

    Cors patch

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require("express");
 const app = express();
 const port = 3080;
 const routes = require("./routes/routes");
+var cors = require("cors");
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
