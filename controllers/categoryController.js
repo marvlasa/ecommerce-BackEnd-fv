3944d0d7f0f0d9e98975422c9f8b5eb9bc4e52b6
Nicolas Curbelo <nicolas.curbelo.sarmiento@gmail.com>
2021-05-19 18:10:40 -0300

    Category Controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,9 +1,46 @@
-const {
-  Category,
-  Status,
-  Client,
-  Admin,
-  Product,
-  Order,
-  OrdersProduct,
-} = require("../database/index");
+const { Category } = require("../database/index");
+
+module.exports = {
+  indexCategories: async (req, res) => {
+    const categories = await Category.findAll();
+    categories.map((article) => {
+      return article;
+    });
+
+    res.json({ categories });
+  },
+
+  create: async (req, res) => {
+    try {
+      const name = req.body.name;
+      console.log(name);
+      const category = await Category.create({
+        name: name,
+      });
+      res.json({ category });
+    } catch (error) {
+      res.status(400);
+    }
+  },
+
+  destroy: async (req, res) => {
+    try {
+      const name = req.body.name;
+      await Category.destroy({ where: { name: name } });
+      res.json("Se ha borrado con exito");
+    } catch (error) {
+      res.status(400);
+    }
+  },
+
+  update: async (req, res) => {
+    try {
+      const name = req.body.name;
+      const newName = req.body.newName;
+      await Category.update({ name: newName }, { where: { name: name } });
+      res.json("Se ha actualizado con exito");
+    } catch (error) {
+      res.status(400);
+    }
+  },
+};
--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const products = require("../database/data");
 
 const clientController = require("../controllers/clientController");
+const categoryController = require("../controllers/categoryController");
 
 router.get("/products", (req, res) => res.json(products));
 //router.get("/product/:id", (req, res) => res.json(products[req.params.id - 1]));
@@ -18,5 +19,10 @@ router.get("/client", clientController.findOneClient);
 router.post("/client", clientController.createClient);
 router.delete("/client", clientController.deleteClient);
 router.patch("/client", clientController.editClient);
+
+router.get("/category", categoryController.indexCategories);
+router.post("/category", categoryController.create);
+router.delete("/category", categoryController.destroy);
+router.patch("/category", categoryController.update);
 
 module.exports = router;
